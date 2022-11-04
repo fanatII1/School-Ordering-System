f5e1e74c313e94f9d7bc4b1cee0210187044293d
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2022-11-04 10:34:44 +0200

    added ordered food items on user profile

--- frontend/school-food-ordering-system/src/Components/UserProfile/UserProfile.jsx
+++ frontend/school-food-ordering-system/src/Components/UserProfile/UserProfile.jsx
@@ -5,27 +5,54 @@ import {useAuth0} from '@auth0/auth0-react'
 
 function UserProfile() {
     const {user, isAuthenticated, isLoading} = useAuth0();
+
     if(isLoading) return <p>Loading...</p>
 
     const {name, picture, email}  = user;
+    //we fetch the 
+    let orderedItems = JSON.parse(localStorage.getItem('Ordered Items'));
+    console.log(orderedItems)
     if(isAuthenticated){
         return (
-            <main id="UserProfile">
+            <main id='UserProfile'>
                 <NavBarMain/>
 
-                <article id="user-content">
-                    <section className="user-info">
-                        <div className="user-image-wrapper">
-                            <img src={picture} alt="" id="user-image" />
+                <article id='user-content'>
+                    <section className='user-info'>
+                        <div className='user-image-wrapper'>
+                            <img src={picture} alt='' id='user-image' />
                         </div>
-                        <div className="name-email">
-                            <h3 className="user-name-heading">
-                                Name : <p className="user-name">{name}</p>
+                        <div className='name-email'>
+                            <h3 className='user-name-heading'>
+                                Name : <p className='user-name'>{name}</p>
                             </h3>
-                            <h3 className="user-email-heading">
-                                Email : <p className="user-email">{email}</p>
+                            <h3 className='user-email-heading'>
+                                Email : <p className='user-email'>{email}</p>
                             </h3>
                         </div>
+                    </section>
+
+                    <section id='user-ordered-items'>
+                        <h2 id='ordered-items-heading'>Ordered Items</h2>
+                        <div className='items-legend'>
+                            <h4 className='item-heading'>Items Name:</h4>
+                            <h4 className='item-heading'>Quantity:</h4>
+                            <h4 className='item-heading'>Price:</h4>
+                        </div>
+                        {
+                            orderedItems === null ? <></> :
+                            orderedItems.map((item)=>{
+                                let [foodName, foodInfoDetails] = item;
+                                let {foodQuantity, price} = foodInfoDetails;
+                                return(
+                                    <div className='ordered-item-wrapper'>
+                                        <p className='ordered-item item-name'>{foodName}</p>
+                                        <p className='ordered-item item-quantity'>{foodQuantity}</p>
+                                        <p className='ordered-item item-price'>R{price}</p>
+                                    </div>
+                                )
+                            })
+                        }
                     </section>
                 </article>
 
