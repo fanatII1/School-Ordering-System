a63e322690058a1ee326d004607d2b4f8b6213fd
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2022-11-04 09:21:32 +0200

    added user profile info

--- frontend/school-food-ordering-system/src/Components/UserProfile/UserProfile.jsx
+++ frontend/school-food-ordering-system/src/Components/UserProfile/UserProfile.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import './UserProfile.css'
+import NavBarMain from '../NavBar_Main/NavBar_Main';
+import {useAuth0} from '@auth0/auth0-react'
 
 function UserProfile() {
-  return (
-    <div>UserProfile</div>
-  )
+    const {user, isAuthenticated, isLoading} = useAuth0();
+    if(isLoading) return <p>Loading...</p>
+
+    const {name, picture, email}  = user;
+    if(isAuthenticated){
+        return (
+            <main id="UserProfile">
+                <NavBarMain/>
+
+                <article id="user-content">
+                    <section className="user-info">
+                        <div className="user-image-wrapper">
+                            <img src={picture} alt="" id="user-image" />
+                        </div>
+                        <div className="name-email">
+                            <h3 className="user-name-heading">
+                                Name : <p className="user-name">{name}</p>
+                            </h3>
+                            <h3 className="user-email-heading">
+                                Email : <p className="user-email">{email}</p>
+                            </h3>
+                        </div>
+                    </section>
+                </article>
+
+            </main>
+          )
+    }
 }
 
 export default UserProfile
