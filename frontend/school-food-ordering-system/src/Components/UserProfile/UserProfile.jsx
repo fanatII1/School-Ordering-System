17291bad142818867ba15fb545a47138bf577158
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2022-11-09 08:32:20 +0200

    ensured that correct user info displays at correct time

--- frontend/school-food-ordering-system/src/Components/UserProfile/UserProfile.jsx
+++ frontend/school-food-ordering-system/src/Components/UserProfile/UserProfile.jsx
@@ -1,10 +1,11 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import './UserProfile.css'
 import NavBarMain from '../NavBar_Main/NavBar_Main';
 import {useAuth0} from '@auth0/auth0-react'
 
 function UserProfile() {
     const {user, isAuthenticated, isLoading} = useAuth0();
+    const [userInfo, setUserInfo] = useState(null)
 
     //when user is authenticated, we check if their data has been loaded
     //if it has, send request to fetch their order from the db
@@ -14,7 +15,9 @@ function UserProfile() {
         }
         else{        
             async function fetchOrders(){
-                const {name}  = user;
+                var {name, email, picture}  = user;
+                setUserInfo({name, email, picture})
+                
                 await fetch('/ProfileOrders', {
                     method: 'POST',
                     headers: {'Content-Type': 'application/json'},
@@ -37,14 +40,14 @@ function UserProfile() {
                 <article id='user-content'>
                     <section className='user-info'>
                         <div className='user-image-wrapper'>
-                            {/* <img src={picture} alt='' id='user-image' /> */}
+                            <img src={userInfo === null ? '' : userInfo.picture} alt='' id='user-image' />
                         </div>
                         <div className='name-email'>
                             <h3 className='user-name-heading'>
-                                {/* Name : <p className='user-name'>{name}</p> */}
+                                Name : <p className='user-name'>{userInfo === null ? '' : userInfo.name}</p>
                             </h3>
                             <h3 className='user-email-heading'>
-                                {/* Email : <p className='user-email'>{email}</p> */}
+                                Email : <p className='user-email'>{userInfo === null ? '' : userInfo.email}</p>
                             </h3>
                         </div>
                     </section>
