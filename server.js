db3706b43d2b6f455ef5daf4e27f803f13210367
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2022-11-16 09:56:25 +0200

    serve static files script

--- server.js
+++ server.js
@@ -98,6 +98,14 @@ app.post('/ProfileOrders', (req, res)=>{
     findSpecificOrder.findSpecificOrder(req, res)
 })
 
+//Serve static files in production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('frontend/school-food-ordering-system/build'))
+    app.get('*', (req,res)=>{
+      res.sendFile(path.resolve(__dirname, 'frontend/school-food-ordering-system', 'build', 'index.html'))
+    })
+  }
+
 //Server listens on PORT 3001 or environment variable PORT
 app.listen(PORT, ()=>{
     console.log(`listenting on port ${PORT}`)
